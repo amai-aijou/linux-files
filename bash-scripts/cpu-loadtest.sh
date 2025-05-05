19e28f04815bb982ed9db7b474d3d3365edad76a
amai-aijou <joseph.russ.olson@gmail.com>
2025-05-04 23:42:24 -0500

    Update cpu-loadtest.sh
    Add for loop to run in batches

--- bash-scripts/cpu-loadtest.sh
+++ bash-scripts/cpu-loadtest.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
 # This Script will place the CPU under high load
 
-dd if=/dev/zero of=test.iso bs=1G count=1;
+#run 10 times
+# use Data Destroyer to create a 1GB backup with a single massive block
+#removes file when complete
+for i in {1..10};
 
-rm test.iso;
+do dd if=/dev/zero of=test.iso bs=1G count=1; rm test.iso;
+
+done
